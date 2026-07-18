2587cc729bf841a2cc3ad1874293b0c95e1e8e54
bekkouame1024 <136476874+bekkouame1024@users.noreply.github.com>
2026-07-18 22:30:41 +0900

    changelogの指定が必須だったっぽいので空文字で代用

--- build.gradle.kts
+++ build.gradle.kts
@@ -164,7 +164,7 @@ publishMods {
     //additionalFiles.from(tasks.remapSourcesJar.map { it.archiveFile.get() })
     displayName = "${property("mod.name")} ${property("mod.version")} for ${property("mod.mc_title")}"
     version = property("mod.version") as String
-    //changelog = rootProject.file("CHANGELOG.md").readText()
+    changelog = /*rootProject.file("CHANGELOG.md").readText()*/""
     type = STABLE
     modLoaders.add("fabric")
 
